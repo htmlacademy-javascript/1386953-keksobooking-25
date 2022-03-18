ee4bc35a7ba1414a6d1cea0685b08b854bd2bba7
Andrei Kozlov <goodcharity@gmail.com>
2022-03-18 18:21:11 +0300

    Попытка реализовать механизм заполнения данными

--- js/card.js
+++ js/card.js
@@ -8,6 +8,7 @@ const adverts = createAdverts();
 
 adverts.forEach(({ author, offer }) => {
   const advert = advertTemplate.cloneNode(true);
+  const advertFeatures = advert.querySelectorAll('.popup__features');
 
   advert.querySelector('.popup__avatar').src = author.avatar;
   advert.querySelector('.popup__title').textContent = offer.title;
@@ -16,9 +17,20 @@ adverts.forEach(({ author, offer }) => {
   advert.querySelector('.popup__type').textContent = offer.type;
   advert.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
   advert.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  advert.querySelector('.popup__features').textContent = offer.features;
+
+  advertFeatures.forEach((advertFeature) => {
+    const isNecessary = offer.features.some((feature) => advertFeature.classList.contains(`popup__feature--${feature}`),);
+
+    if (!isNecessary) {
+      advertFeature.remove();
+    }
+  });
+
   advert.querySelector('.popup__description').textContent = offer.description;
-  advert.querySelector('.popup__photo').src = offer.photos;
+  offer.photos.forEach((photo) => {
+    advert.querySelector('.popup__photo').src = photo;
+  });
+
   advertFragment.append(advert);
 
 });
