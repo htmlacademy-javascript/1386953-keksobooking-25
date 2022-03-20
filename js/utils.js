8435190d49f3c315d7d25519780f8969d828bc02
Andrei Kozlov <goodcharity@gmail.com>
2022-03-20 22:06:35 +0300

    Создал модуль и добавил функций по склонению существительных в множественном числе

---
+++ js/utils.js
@@ -0,0 +1,21 @@
+const pluralize = (value, one, two, five) => {
+  const mod100 = Math.abs(value % 100);
+  if (mod100 > 10 && mod100 < 20) {
+    return five;
+  }
+
+  const mod10 = mod100 % 10;
+  if (mod10 > 1 && mod10 < 5) {
+    return two;
+  }
+
+  return mod10 === 1 ? one : five;
+};
+
+const pluralizeRooms = (rooms) => pluralize(rooms, 'комната', 'комнаты', 'комнат');
+const pluralizeGuestsInGenitive = (guests) => pluralize(guests, 'гостя', 'гостей', 'гостей');
+
+export {
+  pluralizeRooms,
+  pluralizeGuestsInGenitive
+};
