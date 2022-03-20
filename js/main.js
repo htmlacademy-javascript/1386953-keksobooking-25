4859bc3567271d7ab293ebf18a49680572d40d74
Andrei Kozlov <goodcharity@gmail.com>
2022-03-20 22:46:01 +0300

    Добавил импорт для вызова функции создания карточки

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-import './card.js';
+import { createAdverts } from './data.js';
+import { renderCard } from './card.js';
+
+const map = document.querySelector('.map');
+const mapCanvas = map.querySelector('.map__canvas');
+
+const adverts = createAdverts();
+
+mapCanvas.append(renderCard(adverts[0]));
