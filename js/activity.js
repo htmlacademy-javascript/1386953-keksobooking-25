e378cbb581a4bff997b1d6366faa931769d96a10
Andrei Kozlov <goodcharity@gmail.com>
2022-04-02 22:18:44 +0300

    Переместил процедуры удаления обработчиков

--- js/activity.js
+++ js/activity.js
@@ -10,22 +10,18 @@ fieldSets.forEach((fieldSet) => fieldSet.setAttribute('disabled', 'disabled'));
 const enableForm = () => {
   adForm.classList.remove('ad-form--disabled');
   fieldSets.forEach((fieldSet) => fieldSet.removeAttribute('disabled', 'disabled'));
-
-
-  document.removeEventListener('keydown', disableForm);
 };
 
 const disableForm = () => {
   adForm.classList.add('ad-form--disabled');
   fieldSets.forEach((fieldSet) => fieldSet.setAttribute('disabled', 'disabled'));
   // clearCard();
-
-  document.removeEventListener('keydown', enableForm);
 };
 
 document.addEventListener('keydown', (evt) => {
   if (isEnterKey(evt)) {
     enableForm();
+    document.removeEventListener('keydown', disableForm);
   }
 });
 
@@ -33,5 +29,6 @@ document.addEventListener('keydown', (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
     disableForm();
+    document.removeEventListener('keydown', enableForm);
   }
 });
