330af885bdb7cf63d712085372f26076d72ef234
Andrei Kozlov <goodcharity@gmail.com>
2022-04-03 20:42:53 +0300

    Удаление лишнего

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-import { createAdverts } from './data.js';
-import { renderCard } from './card.js';
 import './form.js';
 import './activity.js';
-
-const map = document.querySelector('.map');
-const mapCanvas = map.querySelector('.map__canvas');
-
-const adverts = createAdverts();
-
-mapCanvas.append(renderCard(adverts[0]));
+import './map.js';
